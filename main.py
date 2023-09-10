bd48fc6177e47f684c4faa0d174fa73827c3afc8
Xiang Chen <120740760+xiangchen99@users.noreply.github.com>
2023-09-10 02:41:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID'] = 'AC10b029395650ed8afd05108e47968825'
-auth_token = os.environ['TWILIO_AUTH_TOKEN'] = '6554affbc745cb90bd319d60cd450bd1'
+account_sid = os.environ['TWILIO_ACCOUNT_SID'] = 
+auth_token = os.environ['TWILIO_AUTH_TOKEN'] =
 client = Client(account_sid, auth_token)
 
 # Just use a subset of the classes
